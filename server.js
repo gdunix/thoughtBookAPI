1d0b9c153256317bff3771179991459b9a2f65a6
Georgios Ntounis <giorgosdounis@gmail.com>
2022-12-03 21:31:10 +0200

    Test

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
     if (config.allowCrossOrigin || true) {
         res.header("Access-Control-Allow-Origin", '*');
     }
+    
     res.header("Access-Control-Allow-Origin", 'https://cerulean-brown-bear-fez.cyclic.app/');
     res.header("Access-Control-Request-Headers", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS, DELETE");
